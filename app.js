c8b3e638ae9eebcd2cdd6c065040440e32822263
Sujal Prajapati <prajapatisujal75@gmail.com>
2025-01-27 11:09:31 +0530

    code

---
+++ app.js
@@ -0,0 +1,78 @@
+// const original = { a: 1, b: 2 };
+// const copy = Object.assign({}, original);
+// console.log(copy)
+
+
+// const a=
+// {
+//     a:5,
+//     g:7,
+// }
+
+// const b={...a}
+// console.log(b);
+
+
+// const b=structuredClone(a)
+
+// console.log(b);
+
+
+// const original = { a: 1, b: { c: 2 } };
+// const copy = JSON.parse(JSON.stringify(original));
+// console.log(copy);
+
+
+// 
+
+// let n=false;
+// switch(n){
+//     case 1:
+//         console.log("hello");
+//         break;
+
+//         case 2:
+//             console.log("virat");
+//             break;
+
+//             default:
+//                 console.log("veer")
+// }
+
+
+
+// const array = [1, 2, 3, 4, 4, 5, 6, 2];
+
+// const duplicates = array.filter((item, index) => {
+//     return index;
+// });
+
+
+// // 
+// console.log(duplicates)
+
+// var a=20;
+
+// function square(n){
+//     let ans=n*n;
+//     return ans;
+// }
+// let s=square(a);
+// console.log(s);
+// let m=square(4);
+// console.log(m);
+
+
+//hoisting
+
+
+// const a=55;
+
+// function getname(){
+//     console.log("javascript ")
+//     console.log(a);
+// }
+
+// getname();
+// console.log(a)
+
